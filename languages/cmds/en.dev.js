7b06cbc4d40b3f858fbcd9371de240ae4da0e3d9
Naknakashi <167133980+Naknakashi@users.noreply.github.com>
2024-04-15 20:11:03 +0000

    Update and rename en.js to en.dev.js

--- languages/cmds/en.dev.js
+++ languages/cmds/en.dev.js
@@ -427,7 +427,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "𝒎𝒐𝒏 𝒔𝒚𝒔𝒕è𝒎𝒆 𝒅𝒆 𝒑𝒓𝒆𝒇𝒊𝒙 👉🏽: %1\n🛸 Your box chat prefix: %2"
 		}
 	},
 	rank: {
